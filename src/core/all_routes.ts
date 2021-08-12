7e9b1f6f62ee8a2d68c18d955ba1d2c54f263ebe
sebmandal <sebastian.mandal@icloud.com>
2021-08-12 22:35:45 +0200

    Initial block API setup complete on the backend side of things

--- src/core/all_routes.ts
+++ src/core/all_routes.ts
@@ -1,12 +1,6 @@
 // importing all the pages and APIs
-import Home from "../example/pages/home";
-import About from "../example/pages/about";
-import Contact from "../example/pages/contact";
-import API from "../example/api/employees";
+import blocks from "../api_routes/blocks";
 
 export default {
-	Home,
-	About,
-	Contact,
-	API,
+	blocks,
 };
